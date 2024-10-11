9b2a8d76578b1254670aef5d95acc1c66b5cbcfd
Zhou Hanwen <zhouhanwen2020@sibs.ac.cn>
2024-10-12 02:24:20 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='LaMorena',
+    version='1.0', 
+    author='Rnasys Lab & Bio-Med Big Data Center, CAS', 
+    author_email='zhouhanwen2020@sinh.ac.cn', 
+    url="https://github.com/rnasys/LaMorena",
+    license="MIT",
+    packages=["LaMorena"], 
+    description='A Foundation Language Model For Regulation of RNA'
+)
